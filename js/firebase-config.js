d32869920ffd5597d069294e72c4a06ae9bf3bb4
Mariana <mariana.isaza2004@gmail.com>
2026-07-20 15:54:36 -0500

    Connect site to the Colaboration-Hub Firebase project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,10 +3,10 @@
 // These keys are NOT secret, they are meant to be public (security comes
 // from Firestore/Auth security rules, not from hiding them).
 export const firebaseConfig = {
-  apiKey: "REPLACE_apiKey",
-  authDomain: "REPLACE.firebaseapp.com",
-  projectId: "REPLACE_projectId",
-  storageBucket: "REPLACE.appspot.com",
-  messagingSenderId: "REPLACE_senderId",
-  appId: "REPLACE_appId",
+  apiKey: "AIzaSyDWsGsXj1RSvuS7_xUOYp_gbx8cADYMdIQ",
+  authDomain: "colaboration-hub.firebaseapp.com",
+  projectId: "colaboration-hub",
+  storageBucket: "colaboration-hub.firebasestorage.app",
+  messagingSenderId: "623886245435",
+  appId: "1:623886245435:web:8cdb5360eda0bae78c861e",
 };
